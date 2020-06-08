460abc40bbfb2f318afa20f08094798a37f93583
javan9916 <javaraya9916@gmail.com>
2020-06-08 14:40:05 -0600

    login added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './components/main/main.component'
+import { LoginComponent } from './components/login/login.component'
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
-  { path: 'main', component: MainComponent }
+  { path: 'main', component: MainComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({

eb0132f2a23b62abe14fd5315246e3804b035d8e
Fernando Miranda <fernandoj_10@hotmail.com>
2020-06-15 12:50:22 -0600

    Cambios login/regiones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainComponent } from './components/main/main.component'
-import { LoginComponent } from './components/login/login.component'
+import { MainComponent } from './components/main/main.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
